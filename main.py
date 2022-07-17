c2492b3c788ab70cd7dbb7b788f41ed1ee0adc24
miura227 <101302446+miura227@users.noreply.github.com>
2022-07-18 02:15:05 +0900

    nine

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ import secrets
 
 import re
 
+import random
+
 load_dotenv()
 cred = credentials.Certificate(json.loads(os.getenv("FIREBASE_CRED")))
 firebase_admin.initialize_app(cred)
@@ -71,7 +73,7 @@ def handle_message(event):
         else:
             line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="入力方式が間違っています！\n割り勘<金額>円の形で再送信してください！")
+            TextSendMessage(text="入力方式が間違っているよ！\n割り勘<金額>円の形でもう一度送信してね！")
         )
 
             
@@ -85,12 +87,12 @@ def init_data(event,total_price):
     doc_ref = firestore.collection("payments").document()
     doc_ref.set({
         "group_id":event.source.group_id,
-        "total_price":total_price,
+        "total_price":int(total_price),
         "users":{}
     })
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="https://liff.line.me/1657307954-mJEJ8lW4/?sessionId="+doc_ref.id)
+        TextSendMessage(text="https://liff.line.me/1657307954-mJEJ8lW4/?sessionId="+doc_ref.id+"&totalPrice="+total_price)
     )
 
 @app.route("/payment",methods=['POST'])
@@ -99,8 +101,73 @@ def post_payment():
     doc_ref = firestore.collection('payments').document(request.json["sessionId"])  #扱うドキュメントを指定する
     users_ref = doc_ref.get().to_dict() #ここで取得する
     users_ref["users"][request.json["userId"]] = request.json["userData"]  #ここでkeyに存在しないuserIdを指定した場合に新しくuserIdをkeyに持つ連想配列を追加する。既にuserIdが存在した場合上書きする
+    users_ref["users"][request.json["userId"]]["max_offer"] = True
+    users_ref["users"][request.json["userId"]]["min_offer"] = True
+    for userId in users_ref["users"]:
+        if(users_ref["users"][userId]["max_offer"] == True):
+            if(users_ref["users"][userId]["payment_offer_price"] < users_ref["users"][request.json["userId"]]["payment_offer_price"]):
+                users_ref["users"][userId]["max_offer"] = False
+            elif(users_ref["users"][userId]["payment_offer_price"] != users_ref["users"][request.json["userId"]]["payment_offer_price"]):
+                users_ref["users"][request.json["userId"]]["max_offer"] = False
+        if(users_ref["users"][userId]["min_offer"] == True):
+            if(users_ref["users"][userId]["payment_offer_price"] > users_ref["users"][request.json["userId"]]["payment_offer_price"]):
+                users_ref["users"][userId]["min_offer"] = False
+            elif(users_ref["users"][userId]["payment_offer_price"] != users_ref["users"][request.json["userId"]]["payment_offer_price"]):
+                users_ref["users"][request.json["userId"]]["min_offer"] = False
     doc_ref.update({"users":users_ref["users"]})   #firestoreのusersを上書きする
+    members_count = line_bot_api.get_group_members_count(request.json["groupId"])
+    users_ref = doc_ref.get().to_dict()
+    if(members_count == len(users_ref["users"])):
+        #print(members_count)
+        total_offer_price = 0
+        for userId in users_ref["users"]:
+            total_offer_price += users_ref["users"][userId]["payment_offer_price"]
+        rich_members = []
+        poor_members = []
+        print(users_ref["total_price"])
+        print(type(users_ref["total_price"]))
+        if(total_offer_price >= users_ref["total_price"]):
+            #print(total_offer_price)
+            paymented_price = 0
+            for userId in users_ref["users"]:
+                users_ref["users"][userId]["payment_price"] = users_ref["total_price"] * (users_ref["users"][userId]["payment_offer_price"] / total_offer_price) // max(pow(10,len(str(users_ref["total_price"]))-3),1)*max(pow(10,len(str(users_ref["total_price"]))-3),1)
+                paymented_price += users_ref["users"][userId]["payment_price"]
+                if users_ref["users"][userId]["max_offer"] == True:
+                    rich_members.append(userId)
+                if users_ref["users"][userId]["min_offer"] == True:
+                    poor_members.append(userId)
+            for rich_member in rich_members:
+                users_ref["users"][rich_member]["payment_price"] += users_ref["total_price"] % max(pow(10,len(str(users_ref["total_price"]))-3),1) // len(rich_members)
+                paymented_price += users_ref["total_price"] % max(pow(10,len(str(users_ref["total_price"]))-3),1) // len(rich_members)
+            #print(paymented_price)
+            users_ref["users"][random.choice(rich_members)]["payment_price"] += users_ref["total_price"] - paymented_price
+            users_ref["users"]["rich_members"] = rich_members
+            users_ref["users"]["poor_members"] = poor_members
+            doc_ref.update({"users":users_ref["users"]})
+            #print(total_offer_price)
+
+            if len(users_ref["users"]) == len(rich_members):
+                white_message = "みんな同じ金額を支払いました！"
+                line_bot_api.push_message(users_ref["group_id"], TextSendMessage(text=white_message))
+            else:
+                black_message = "一番多く払った人\n"
+                for rich_member in rich_members:
+                    black_message += "  " + users_ref["users"][rich_member]["user_name"] + " : " + str(int(users_ref["users"][rich_member]["payment_price"])) + "円\n"
+                black_message += "一番払わなかった人\n"
+                for poor_member in poor_members:
+                    black_message += "  " + users_ref["users"][poor_member]["user_name"] + " : " + str(int(users_ref["users"][poor_member]["payment_price"])) + "円\n"
+                line_bot_api.push_message(users_ref["group_id"], TextSendMessage(text=black_message))
+            #line_bot_api.push_message(users_ref["group_id"], TextSendMessage(users_ref["users"][userId]["min_offer"]))
+                #print(users_ref)
+            line_bot_api.push_message(users_ref["group_id"],TextSendMessage(text="https://liff.line.me/1657307954-mJEJ8lW4/result/?sessionId="+doc_ref.id))
+        else:
+            #print(total_offer_price)
+            doc_ref.update({"users":{}}) 
+            resend_message = "全員の合計が支払い金額に届かなかったよ！再度入力し直してね！\nヒント：少し大きめの額にして太っ腹なところを見せつけよう！\n\nhttps://liff.line.me/1657307954-mJEJ8lW4/?sessionId="+doc_ref.id
+            line_bot_api.push_message(users_ref["group_id"], TextSendMessage(text=resend_message))
     return "success",200
+
+
 
 @app.route("/init_data/<group_id>",methods=['GET'])
 def fetch_user_data(group_id):
@@ -121,7 +188,7 @@ def create_doc():
 @app.route("/test2",methods=["GET"])
 def read_doc():
     payments_ref = firestore.collection("payments").document("2QY4a5P14XDkao7EQ0C6")
-    payments_ref.update({"users":{"userId2":{"userName":"userName","paymentOfferPrice":5000}}})
+    payments_ref.update({"users":{"userId2":{"userName":"userName",'paymentOfferPrice':5000}}})
     #print(docs.to_dict())
     return "su",200
 
